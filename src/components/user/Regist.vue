76ca79b334da624d73dad7c8a93535b816843b72
becooliu <243257686@qq.com>
2024-05-31 18:19:11 +0800

    Add regist account module

---
+++ src/components/user/Regist.vue
@@ -0,0 +1,110 @@
+<template>
+  <h3 class="title">注册</h3>
+  <el-form ref="ruleFormRef" style="max-width: 600px" :model="ruleForm" status-icon :rules="rules" label-width="auto"
+    class="demo-ruleForm">
+    <el-form-item label="账号" prop="username">
+      <el-input v-model="ruleForm.username" type="text" autocomplete="off" />
+    </el-form-item>
+    <el-form-item label="密码" prop="password">
+      <el-input v-model="ruleForm.password" type="password" autocomplete="off" />
+    </el-form-item>
+    <el-form-item label="Confirm" prop="checkPass">
+      <el-input v-model="ruleForm.checkPass" type="password" autocomplete="off" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm(ruleFormRef)"> Submit </el-button>
+      <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import instance from '@/axios/base'
+// import type { FormInstance, FormRules } from 'element-plus'
+
+const ruleFormRef = ref(null)
+
+const validateUsername = (rule: any, value: String, callback: any) => {
+  if (value === '') {
+    callback(new Error('用户名不能为空。'))
+  }
+  callback()
+}
+
+const validatePass = (rule: any, value: any, callback: any) => {
+  if (value === '') {
+    callback(new Error('请输入密码'))
+  } else {
+    if (ruleForm.checkPass !== '') {
+      if (!ruleFormRef.value) return
+      ruleFormRef.value.validateField('checkPass')
+    }
+    callback()
+  }
+}
+const validatePass2 = (rule: any, value: any, callback: any) => {
+  if (value === '') {
+    callback(new Error('请再次输入密码'))
+  } else if (value !== ruleForm.password) {
+    callback(new Error('两次密码不一致!'))
+  } else {
+    callback()
+  }
+}
+
+const ruleForm = reactive({
+  username: '',
+  password: '',
+  checkPass: ''
+})
+
+const rules = reactive({
+  username: [{ validator: validateUsername, trigger: 'blur' }],
+  password: [{ validator: validatePass, trigger: 'blur' }],
+  checkPass: [{ validator: validatePass2, trigger: 'blur' }]
+})
+
+const submitForm = (formEl) => {
+  if (!formEl) return
+  formEl.validate((valid) => {
+    if (valid) {
+      const data = {
+        username: ruleForm.username,
+        password: ruleForm.password
+      }
+      instance.post('/user/create', data)
+      .then(res => {
+        const {status, message} = res?.data
+        const type = status === 200 ? 'success' : 'error'
+        if(res?.status === 200) {
+          console.log(res)
+          ElNotification({
+            title: '注册帐号',
+            message: message,
+            type,
+          })
+        }
+      }).catch(err => {
+        ElNotification({
+          title: '注册帐号',
+          message: err,
+          type: 'error',
+        })
+        throw new Error(err)
+      })
+    } else {
+      console.log('error submit!')
+    }
+  })
+}
+
+const resetForm = (formEl) => {
+  if (!formEl) return
+  formEl.resetFields()
+}
+</script>
+
+<style scoped>
+  
+</style>
