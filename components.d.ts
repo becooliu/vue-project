4c542cd85e0e059893db10bd8d5599470cde85d4
becooliu <243257686@qq.com>
2024-07-09 18:57:06 +0800

    新增denied 页面

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    Denied: typeof import('./src/components/denied/index.vue')['default']
     ElAvatar: typeof import('element-plus/es')['ElAvatar']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCol: typeof import('element-plus/es')['ElCol']
