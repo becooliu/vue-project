de5a8d2dae117b6604ec7fce744f76cf3e27d691
becooliu <243257686@qq.com>
2024-07-09 19:00:18 +0800

    修改状态相关的数据

--- src/store/index.js
+++ src/store/index.js
@@ -2,23 +2,60 @@ import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
 
 export const useUserStatusStore = defineStore('userStatus', () => {
+  // 是否登录
   const isLogin = ref(false)
+
+  // 是否是管理员
   const isAdmin = ref(false)
+
+  // 角色
+  const roles = ref('admin')
+
+  // 权限
+  const userPermissions = ref([])
+
+  // 获取登录状态
   const getLoginStatus = computed(() => {
-    return localStorage.getItem('userKey')?.length || isLogin
+    return localStorage.getItem('userKey')?.length > 0 || isLogin
   })
-  function afterLogin(userStoreData) {
+
+  const getUserIsAdmin = computed(() => {
+    return localStorage.getItem('userKey') === 'admin'
+  })
+
+  // 设置状态（传入权限信息赋值给该状态）
+  const setUserPermissions = params => {
+    userPermissions.value = params
+  }
+
+  const setUserRole = role => {
+    roles.value = role
+  }
+
+  //设置登录后的状态
+  const afterLogin = userStoreData => {
     isLogin.value = true
     isAdmin.value = userStoreData.isAdmin
-    localStorage.setItem('userKey', username)
+    localStorage.setItem('userKey', userStoreData.username)
   }
 
-  function userLogout() {
+  //设置登出后的状态
+  const userLogout = () => {
     isLogin.value = false
-    localStorage.removeItem('userKey')
   }
 
-  return { isLogin, getLoginStatus, afterLogin, userLogout }
+  return {
+    isLogin,
+    isAdmin,
+    roles,
+    userPermissions,
+    getLoginStatus,
+    getUserIsAdmin,
+    setUserPermissions,
+    setUserRole,
+    afterLogin,
+    userLogout
+  }
 })
 
 /* export const useUserStatusStore = defineStore('userStatus', {
