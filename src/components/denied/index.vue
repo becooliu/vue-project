23e3a33d41725da8a0938ab00ee5ed49d3d734de
becooliu <243257686@qq.com>
2024-07-09 18:58:58 +0800

    新增denied 页面

---
+++ src/components/denied/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="denied-box">
+        <h3>您没有权限访问该页面，如有疑问，请联系管理员。</h3>
+    </div>
+</template>
+<style scoped lang="scss">
+    .denied-box {
+        margin-top: 20px;
+        line-height: 30px;
+        text-align: center;
+    }
+</style>
