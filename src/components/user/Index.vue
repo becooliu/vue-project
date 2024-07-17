e018802405b1df664291bba4e26a46408e83b07c
becooliu <243257686@qq.com>
2024-07-17 19:42:51 +0800

    修改组件的引用

--- src/components/user/Index.vue
+++ src/components/user/Index.vue
@@ -1,25 +1,9 @@
 <template>
-    <div class="user-box">
-      <el-avatar> {{username.slice(0,1).toUpperCase()}} </el-avatar>
-      <span class="user-welcome-text">welcome , {{username}}</span>
-    </div>
+    <UserNav></UserNav>
 </template>
 
 <script setup>
-    /* import {useUserStatusStore} from '@/store/index'
-    const store = useUserStatusStore() */
-    const username = localStorage.getItem('userKey')
-
 </script>
 
 <style lang="scss">
-    .user-box {
-        font-weight: 700;
-        display: flex;
-        align-items: center;
-        .user-welcome-text {
-            margin-left: 5px;
-            font-weight: 400;
-        }
-    }
 </style>
