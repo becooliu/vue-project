753170bf8cd86d079b07186a4b53f7df776fb6db
becooliu <243257686@qq.com>
2024-07-17 19:38:22 +0800

    修改组件结构

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,20 @@
 <template>
-  <el-menu
-    :default-active="activeIndex"
-    class="el-menu-demo"
-    mode="horizontal"
-    :ellipsis="false"
-    router
-  >
-    <el-menu-item index="/">首页 </el-menu-item>
-    <div class="flex-grow" />
-    <!-- <el-menu-item index="1">Processing Center</el-menu-item> -->
-    <el-sub-menu index="2">
-      <template #title>用户管理</template>
-        <template v-if="getUserIsAdmin && getLoginStatus">
-          <el-menu-item index="/user/userlist">用户列表</el-menu-item>
-          <el-menu-item index="2-3">重置密码</el-menu-item>
-        </template>
-        <el-menu-item index="/user/regist">注册账号</el-menu-item>
-        <el-menu-item index="/user/login" v-if="!getLoginStatus">用户登录</el-menu-item>
-      <el-menu-item index="/user/login" v-if="getLoginStatus" @click="logout">登出</el-menu-item>
-      <!-- <el-sub-menu index="2-4">
-        <template #title>item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-sub-menu> -->
-    </el-sub-menu>
-    <el-link href="/user/index" target="_self" v-if="!getUserIsAdmin && getLoginStatus">返回用户首页</el-link>
-  </el-menu>
-  <RouterView />
+    <el-row>
+      <el-col :span="24">
+        <router-view></router-view>
+      </el-col>
+    </el-row>
 </template>
 
-<script lang="ts" setup>
-import { RouterView } from "vue-router";
-import { ref } from "vue";
-import {  deleteCookie } from "./utils";
-import { storeToRefs } from 'pinia'
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useUserStatusStore } from './store';
 
-// 引入用户相关的state
-import {useUserStatusStore} from '@/store/index'
 const store = useUserStatusStore()
-const { getLoginStatus, getUserIsAdmin } = storeToRefs(store)
+const {getLoginStatus} = storeToRefs(store)
+console.log('getLoginStatus', getLoginStatus)
 
-console.log('App store', getLoginStatus.value, getUserIsAdmin.value)
-
-const activeIndex = ref("1");
-
-function setLogoutData(){
-  localStorage.removeItem('userKey')
-  deleteCookie('userInfo')
-}
-
-function logout() {
-  store.userLogout()
-  setLogoutData()
-
-  ElNotification({
-    title: "帐号登出",
-    message: '账号登出成功',
-    type: "success"
-  })
-}
+/* const isLogin = store.getLoginStatus
+console.log('App.vue isLogin', isLogin) */
 
 </script>
-
-<style>
-.flex-grow {
-  flex-grow: 1;
-}
-</style>
