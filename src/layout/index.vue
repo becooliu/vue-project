aa7ad5e1fb6793a242a6d9bf85c351d6498fe7c2
becooliu <243257686@qq.com>
2024-07-23 15:05:26 +0800

    修改layout 样式，修改页面布局内组件

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,12 +3,15 @@
         <el-container>
             <el-aside>
                 <el-scrollbar class="scrollbar">
-                <!-- 菜单组件-->
+                    <!-- 菜单组件-->
                     <Menu />
                 </el-scrollbar>
             </el-aside>
             <el-container>
-                <el-header>Header</el-header>
+                <el-header class="layout-header">
+                    <Breadcrumb />
+                    <UserNavComp />
+                </el-header>
                 <el-main>
                     <router-view></router-view>
                 </el-main>
@@ -18,27 +21,41 @@
 </template>
 
 <script setup>
-  import Menu from '@/layout/components/menu/index.vue'
+import Menu from '@/layout/components/menu/index.vue'
+import Breadcrumb from './components/breadcrumb/bread.vue'
+import UserNavComp from '@/layout/components/usernav/UserNavComp.vue'
 </script>
 
 <style scoped lang="scss">
-  .common-layout {
+@import '@/styles/variable.scss';
+
+.common-layout {
     width: 100%;
     height: 100vh;
+
     .sys-title {
         text-align: center;
     }
+
     .el-aside {
         width: 260px;
         height: 100vh;
-        background-color: #ccc;
+        border-right: 1px solid #ccc;
+
         .el-menu--vertical {
             height: 100vh;
         }
     }
+
+    .layout-header {
+        display: flex;
+        justify-content: space-between;
+    }
+
     .el-header {
-        height: 50px;
-        background-color: #aaa;
+        height: $base-header-height;
+        line-height: $base-header-height;
+        border-bottom: 1px solid #ccc;
     }
     .el-main {
         // background-color: #bbb;
