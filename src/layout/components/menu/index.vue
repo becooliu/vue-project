e3474ab7d044061cb2e2676db877c27e6d303eb4
becooliu <243257686@qq.com>
2024-07-23 15:08:02 +0800

    修改菜单组件

--- src/layout/components/menu/index.vue
+++ src/layout/components/menu/index.vue
@@ -1,11 +1,6 @@
 <template>
     <h5 class="sys-title">博客管理 V1.0</h5>
-    <el-menu 
-        router 
-        default-active="1" 
-        :default-openeds="['1']" 
-        class="el-menu-vertical-demo" 
-        active-color="#409eff">
+    <el-menu router default-active="1" :default-openeds="['1']" class="el-menu-vertical-demo" active-color="#409eff">
         <el-sub-menu index="1">
             <template #title>
                 <el-icon>
@@ -14,66 +9,70 @@
                 <span>Dashboard</span>
             </template>
             <el-menu-item index="/admin/userlist">数据总览</el-menu-item>
-            <el-menu-item index="2-3">分析</el-menu-item>
+            <el-menu-item index="/getApiVisited">分析</el-menu-item>
         </el-sub-menu>
         <el-sub-menu index="2">
             <template #title>
-                <el-icon><Menu /></el-icon>
+                <el-icon>
+                    <Menu />
+                </el-icon>
                 <span>分类管理</span>
             </template>
-            <el-menu-item index="/category/create">
-                <el-icon><Plus /></el-icon>
+            <el-menu-item index="/blog_category/add">
+                <el-icon>
+                    <Plus />
+                </el-icon>
                 <span>分类添加</span>
             </el-menu-item>
             <el-menu-item index="/category/update">
-                <el-icon><Edit /></el-icon>
+                <el-icon>
+                    <Edit />
+                </el-icon>
                 <span>分类修改</span>
             </el-menu-item>
             <el-menu-item index="/category/remove">
-                <el-icon><Delete /></el-icon>
+                <el-icon>
+                    <Delete />
+                </el-icon>
                 <span>分类删除</span>
             </el-menu-item>
         </el-sub-menu>
         <el-sub-menu index="3">
             <template #title>
-                <el-icon><Management /></el-icon>
+                <el-icon>
+                    <Management />
+                </el-icon>
                 <span>角色管理</span>
             </template>
             <el-menu-item index="/roles/create">
-                <el-icon><Plus /></el-icon>
+                <el-icon>
+                    <Plus />
+                </el-icon>
                 <span>角色添加</span>
             </el-menu-item>
             <el-menu-item index="/roles/update">
-                <el-icon><Edit /></el-icon>
+                <el-icon>
+                    <Edit />
+                </el-icon>
                 <span>角色修改</span>
             </el-menu-item>
             <el-menu-item index="/roles/remove">
-                <el-icon><Delete /></el-icon>
+                <el-icon>
+                    <Delete />
+                </el-icon>
                 <span>角色删除</span>
             </el-menu-item>
         </el-sub-menu>
         <el-sub-menu index="4">
             <template #title>
-                <el-icon><Avatar /></el-icon>
+                <el-icon>
+                    <Avatar />
+                </el-icon>
                 <span>用户管理</span>
             </template>
             <el-menu-item index="/admin/userlist">用户列表</el-menu-item>
             <el-menu-item index="2-3">重置密码</el-menu-item>
-            <el-sub-menu index="1-4">
-                <template #title>item four</template>
-                <el-menu-item index="1-4-1">item one</el-menu-item>
-            </el-sub-menu>
         </el-sub-menu>
-        <el-menu-item index="5">
-            <el-icon><icon-menu /></el-icon>
-            <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="6" disabled>
-            <el-icon>
-                <document />
-            </el-icon>
-            <span>Navigator Three</span>
-        </el-menu-item>
     </el-menu>
 </template>
 
