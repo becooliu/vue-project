c990008fba077d455bf3c31bdd6858e15d251a7a
becooliu <243257686@qq.com>
2024-08-27 17:14:54 +0800

    修改api visited 首页布局

--- src/layout/components/apivisited/index.vue
+++ src/layout/components/apivisited/index.vue
@@ -1,13 +1,22 @@
 <template>
-    <el-card style="max-width: 480px">
-        <template #header>
-            <div class="card-header">
-                <span>接口访问总数</span>
-            </div>
-        </template>
-        <p class="text item">{{ apiVisitedCount }}</p>
-    </el-card>
-    <BlogCount />
+    <el-row :gutter="20">
+        <el-col :span="8">
+            <div class="grid-content ep-bg-purple" />
+            <el-card style="max-width: 480px">
+                <template #header>
+                    <div class="card-header">
+                        <span>接口访问总数</span>
+                    </div>
+                </template>
+                <p class="text item">{{ apiVisitedCount }}</p>
+            </el-card>
+        </el-col>
+
+        <el-col :span="8">
+            <div class="grid-content ep-bg-purple" />
+            <BlogCount />
+        </el-col>
+    </el-row>
 </template>
 
 <script setup>
