2fdca35733539cfa392951d3a032647485f5e5a1
becooliu <243257686@qq.com>
2024-08-27 17:14:08 +0800

    修改博客分类统计接口

--- src/layout/components/apivisited/BlogCount.vue
+++ src/layout/components/apivisited/BlogCount.vue
@@ -2,10 +2,10 @@
     <el-card style="max-width: 480px">
         <template #header>
             <div class="card-header">
-                <span>博客分类统计</span>
+                <span>博客总数</span>
             </div>
         </template>
-        <p class="text item"></p>
+        <p class="text item">{{ blogAmount }}</p>
     </el-card>
 </template>
 
@@ -13,9 +13,9 @@
 import instance from "@/axios/base";
 import { ref } from 'vue'
 
-// const apiVisitedCount = ref(0)
+const blogAmount = ref(0)
 instance.get('/blog/count').then(res => {
-    console.log(res.data)
-    // apiVisitedCount.value = res.data.count
+    const countData = res.data
+    blogAmount.value = countData.reduce((acc, cur) => { acc += cur.count; return acc }, 0)
 })
 </script>
