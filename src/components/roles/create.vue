6c0de3c6c716e1d1d690c40386fcbec409634e53
becooliu <243257686@qq.com>
2024-10-24 18:37:16 +0800

    新增角色--待完成

--- src/components/roles/create.vue
+++ src/components/roles/create.vue
@@ -1,12 +1,31 @@
 <template>
   <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules" label-width="auto" class="custom-ruleForm">
     <h3 class="user_form_title">创建角色</h3>
-    <el-form-item label="角色" prop="role">
-      <el-input v-model="ruleForm.role" type="text" autocomplete="off" />
+    <el-text class="role-tips" type="info">当前已有角色：admin, editor, user, viewer</el-text>
+    <br />
+    <el-form-item label="角色">
+      <!-- <el-input v-model="ruleForm.role" type="text" autocomplete="off" /> -->
+
+      <el-select v-model="ruleForm" placeholder="Select" style="width: 240px">
+        <el-option v-for="item in ruleForm" :key="item._id" :label="item.role" :value="item.role" />
+        <template #footer>
+          <el-button v-if="!isAdding" text bg size="small" @click="onAddOption">
+            Add an option
+          </el-button>
+          <template v-else>
+            <el-input v-model="optionName" class="option-input" placeholder="input option name" size="small" />
+            <el-button type="primary" size="small" @click="onConfirm">
+              confirm
+            </el-button>
+            <el-button size="small" @click="clear">cancel</el-button>
+          </template>
+        </template>
+      </el-select>
+
     </el-form-item>
-    <el-form-item label="权限" prop="permissions">
+    <!-- <el-form-item label="权限" prop="permissions">
       <el-input v-model="ruleForm.permissions" type="text" autocomplete="off" />
-    </el-form-item>
+    </el-form-item> -->
 
     <el-form-item>
       <el-button type="primary" @click="submitForm(ruleFormRef)">
@@ -16,81 +35,123 @@
     </el-form-item>
   </el-form>
 </template>
-  
-  <script lang="ts" setup>
-  import { reactive, ref } from "vue";
-  import instance from "@/axios/base";
-  
-  const ruleFormRef = ref(null);
-  
-  
-  const ruleForm = reactive({
-    role: "",
-    permissions: ""
-  });
-  
-  const validateRole = (rule, value, callback) => {
-    if (value === "") {
-      callback(new Error("角色不能为空。"));
-    }
-    callback();
+
+<script lang="ts" setup>
+import { reactive, ref } from "vue";
+import instance from "@/axios/base";
+
+const ruleFormRef = ref(null);
+
+
+const ruleForm = ref(null)
+const isAdding = ref(false)
+
+// const initialRoles = ref([])
+
+const getAllRoles = async () => {
+
+  const roles = await instance.get('/roles/getAll')
+  // console.log('33333', roles)
+  if (!roles?.data?.length) return
+
+  ruleForm.value = roles.data
+}
+
+getAllRoles()
+
+/* function getInitialRoles() {
+
+  if (ruleForm.value) {
+    ruleForm.value.every(item => { initialRoles.value.push(item.role) })
+    console.log('initialRoles', initialRoles)
   }
-  const validatePermissions = (rule, value, callback) => {
-    if (value === "") {
-      callback(new Error("角色名称不能为空。"));
-    }
-    callback();
+}
+getInitialRoles() */
+
+const onAddOption = () => {
+  isAdding.value = true
+}
+
+const optionName = ref('')
+const onConfirm = () => {
+  if (optionName.value) {
+    ruleForm.value.push({
+      role: optionName.value,
+      permissions: optionName.value,
+    })
+    clear()
   }
-  
-  
-  const rules = reactive({
-    role: [{ validator: validateRole, trigger: "blur" }],
-    permissions: [{ validator: validatePermissions, trigger: "blur" }],
-  });
-  
-  const submitForm = (formEl) => {
-    if (!formEl) return;
-    formEl.validate((valid) => {
-      if (valid) {
-        const data = {
-          role: ruleForm.role,
-          permissions: ruleForm.permissions
-        };
-        instance
-          .post("/roles/create", data)
-          .then((res) => {
-            const { status, message } = res.data;
-            const type = status === 200 ? "success" : "error";
-            if (res?.status === 200) {
-              console.log(res);
-              // eslint-disable-next-line no-undef
-              ElNotification({
-                title: "创建角色",
-                message: message,
-                type,
-              });
-            }
-          })
-          .catch((err) => {
+}
+
+const clear = () => {
+  optionName.value = ''
+  isAdding.value = false
+}
+
+const validateRole = (rule, value, callback) => {
+  if (value === "") {
+    callback(new Error("角色不能为空。"));
+  }
+  callback();
+}
+const validatePermissions = (rule, value, callback) => {
+  if (value === "") {
+    callback(new Error("角色名称不能为空。"));
+  }
+  callback();
+}
+
+
+const rules = reactive({
+  role: [{ validator: validateRole, trigger: "blur" }],
+  permissions: [{ validator: validatePermissions, trigger: "blur" }],
+});
+
+const submitForm = (formEl) => {
+  if (!formEl) return;
+  formEl.validate((valid) => {
+    if (valid) {
+      const data = ruleForm;
+      instance
+        .post("/roles/create", data)
+        .then((res) => {
+          const { status, message } = res.data;
+          const type = status === 200 ? "success" : "error";
+          if (res?.status === 200) {
+            console.log(res);
             // eslint-disable-next-line no-undef
             ElNotification({
               title: "创建角色",
-              message: err,
-              type: "error",
+              message: message,
+              type,
             });
-            throw new Error(err);
+          }
+        })
+        .catch((err) => {
+          // eslint-disable-next-line no-undef
+          ElNotification({
+            title: "创建角色",
+            message: err,
+            type: "error",
           });
-      } else {
-        console.log("error submit!");
-      }
-    });
-  };
-  
-  const resetForm = (formEl) => {
-    if (!formEl) return;
-    formEl.resetFields();
-  };
-  </script>
-  
-  <style scoped></style>
+          throw new Error(err);
+        });
+    } else {
+      console.log("error submit!");
+    }
+  });
+};
+
+const resetForm = (formEl) => {
+  if (!formEl) return;
+  formEl.resetFields();
+};
+</script>
+
+<style lang="scss" scoped>
+.option-input {
+  width: 100%;
+  margin-bottom: 8px;
+}
+</style>
   
