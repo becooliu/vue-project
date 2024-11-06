3a7da3d81a4b5ab95fcef45a055dea7ffa383516
becooliu <243257686@qq.com>
2024-11-06 17:42:25 +0800

    完成新增角色功能

--- src/components/roles/create.vue
+++ src/components/roles/create.vue
@@ -1,66 +1,33 @@
 <template>
   <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules" label-width="auto" class="custom-ruleForm">
     <h3 class="user_form_title">创建角色</h3>
-    <el-text class="role-tips" type="info">当前已有角色：admin, editor, user, viewer</el-text>
-    <br />
-    <el-form-item label="角色">
-      <!-- <el-input v-model="ruleForm.role" type="text" autocomplete="off" /> -->
 
-      <el-select v-model="roleName" placeholder="Select" @change="roleChange" style="width: 240px">
-        <el-option v-for="item in ruleForm" :key="item._id" :label="item.role" :value="item.role" />
-        <template #footer>
-          <el-button v-if="!isAdding" text bg size="small" @click="onAddOption">
-            添加新角色
-          </el-button>
-          <template v-else>
-            <el-input v-model="optionName" class="option-input" placeholder="input option name" size="small" />
-            <el-button type="primary" size="small" @click="onConfirm">
-              确认
-            </el-button>
-            <el-button size="small" @click="clear">取消</el-button>
-          </template>
-        </template>
-      </el-select>
+    <div class="header">
+      <el-text class="role-tips" type="success">当前已有角色：</el-text>
+      <el-tag v-for="item in ruleForm" :key="item._id" class="role-tag">{{ item.role }}</el-tag>
+    </div>
 
-    </el-form-item>
-    <el-form-item label="权限" prop="permissions">
-      <el-tag v-for="tag in currentPermission" class="permission-tag" :key="tag" @close="handleClose(tag)" closable>
-        {{ tag }}
-      </el-tag>
-      <el-input v-if="inputVisible" class="new-permission-input" ref="inputRef" v-model="inputValue"
-        @keyup.enter="handleInputConfirm" @blur="handleInputConfirm" />
-
-      <el-button v-else class="button-new-tag" type="primary" size="small" @click="showInput">添加权限</el-button>
+    <el-form-item label="角色" prop="role">
+      <el-input v-model="newRole" type="text" autocomplete="off" />
     </el-form-item>
 
     <el-form-item>
-      <el-button type="primary" @click="submitForm(ruleFormRef)">
-        确认修改
-      </el-button>
+      <el-button type="primary" @click="submitForm()">新增角色</el-button>
       <el-button @click="resetForm(ruleFormRef)">重置</el-button>
     </el-form-item>
   </el-form>
 </template>
 
 <script lang="ts" setup>
-import { reactive, ref, nextTick } from "vue";
-import type { CheckboxValueType } from 'element-plus'
+import { reactive, ref } from "vue";
 import instance from "@/axios/base";
-import type { InputInstance } from "element-plus";
 
 const ruleFormRef = ref(null);
-const roleName = ref<CheckboxValueType[]>([])
 
 const ruleForm = ref(null)
-const isAdding = ref(false)
 
-//权限相关
-const inputValue = ref('')
-const inputVisible = ref(false)
-const inputRef = ref<InputInstance>()
-
-
-// const initialRoles = ref([])
+//角色相关
+const newRole = ref('')
 
 function getAllRoles() {
   instance.get('/roles/getAll').then(res => {
@@ -74,54 +41,6 @@ function getAllRoles() {
 
 getAllRoles()
 
-const onAddOption = () => {
-  isAdding.value = true
-}
-
-const optionName = ref('')
-const onConfirm = () => {
-  if (optionName.value) {
-    ruleForm.value.push({
-      role: optionName.value,
-      permissions: optionName.value,
-    })
-    clear()
-  }
-}
-
-const clear = () => {
-  optionName.value = ''
-  isAdding.value = false
-}
-
-// role 变化同步permission
-const currentPermission = ref(null)
-function roleChange(e) {
-  console.log('ruleForm.value', ruleForm.value)
-  currentPermission.value = ruleForm.value.find(item => item?.role == e) ? ruleForm.value.find(item => item?.role == e)?.permissions : ''
-  console.log('currentPermission', currentPermission.value)
-}
-
-//权限相关操作
-const handleClose = (tag: string) => {
-  currentPermission.value.splice(currentPermission.value.indexOf(tag), 1)
-}
-
-const showInput = () => {
-  inputVisible.value = true
-  nextTick(() => {
-    inputRef.value!.input!.focus()
-  })
-}
-
-const handleInputConfirm = () => {
-  if (inputValue.value) {
-    currentPermission.value.push(inputValue.value)
-  }
-  inputVisible.value = false
-  inputValue.value = ''
-}
-
 // 验证操作
 const validateRole = (rule, value, callback) => {
   if (value === "") {
@@ -129,30 +48,45 @@ const validateRole = (rule, value, callback) => {
   }
   callback();
 }
-const validatePermissions = (rule, value, callback) => {
-  if (value === "") {
-    callback(new Error("角色名称不能为空。"));
-  }
-  callback();
-}
-
 
 const rules = reactive({
-  role: [{ validator: validateRole, trigger: "blur" }],
-  permissions: [{ validator: validatePermissions, trigger: "blur" }],
+  role: [{ required: true, validator: validateRole, trigger: "blur" }]
 });
 
-const submitForm = (formEl) => {
-  if (!formEl) return;
-  formEl.validate((valid) => {
-    if (valid) {
-      const data = ruleForm
-      console.log('AAAAAA', data)
-      return;
+const submitForm = () => {
+  if (!newRole.value.length) {
+    ElNotification({
+      title: "新增角色失败",
+      message: "没有角色数据，请确认。",
+      type: "error"
+    })
+    return
+  }
+  const params = {
+    role: newRole.value
+  }
+
+  instance.post('/roles/add', params).then((result) => {
+    const res = result.data
+    if (res.status === 200) {
+      ruleForm.value.push({
+        _id: res.roleData._id,
+        role: res.roleData.role
+      })
+
+      ElNotification({
+        title: "新增角色成功",
+        message: `新增角色 ${res.roleData.role} 成功`,
+        type: "success"
+      })
     } else {
-      console.log("error submit!");
+      ElNotification({
+        title: "新增角色失败",
+        message: `${res.message}`,
+        type: "error"
+      })
     }
-  });
+  })
 };
 
 const resetForm = (formEl) => {
@@ -162,7 +96,12 @@ const resetForm = (formEl) => {
 </script>
 
 <style lang="scss" scoped>
-.permission-tag {
+.header {
+  margin-bottom: 1em;
+}
+
+.permission-tag,
+.role-tag {
   margin-right: 0.5em;
 }
 
