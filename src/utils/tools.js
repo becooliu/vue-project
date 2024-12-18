455834a15330d52971aa53edd01097aafcf486f1
becooliu <243257686@qq.com>
2024-12-18 18:49:54 +0800

    新增获取日期的函数

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -28,3 +28,14 @@ export const deleteCookie = name => {
 export const dateToLocaleString = str => {
   return new Date(str).toLocaleString()
 }
+
+export const timeStringToDate = timestamp => {
+  timestamp = Number(timestamp)
+  let date = new Date(timestamp)
+  let curYear = new Date().getFullYear()
+  let Y = date.getFullYear() == curYear ? '' : curYear + '-'
+  let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-'
+  console.log('M', M)
+  let D = date.getDate() + ''
+  return Y + M + D
+}
