8c38e1924bfdfe36be27a1c19e18df2a059b6283
becooliu <243257686@qq.com>
2025-03-17 18:24:12 +0800

    新增24小时接口访问量统计组件

---
+++ src/layout/components/apivisited/Last24HoursApiVisited.vue
@@ -0,0 +1,20 @@
+<template>
+    <el-card style="max-width: 480px" shadow="hover">
+        <template #header>
+            <div class="card-header">
+                <span>24小内接口访问量</span>
+            </div>
+        </template>
+        <p class="text item custom">{{ last24HoursVisited }}</p>
+    </el-card>
+</template>
+
+<script setup>
+import instance from "@/axios/base";
+import { ref } from 'vue'
+
+const last24HoursVisited = ref(0)
+instance.get('/get24HoursApiVisited').then(res => {
+    last24HoursVisited.value = res.data.count
+})
+</script>
